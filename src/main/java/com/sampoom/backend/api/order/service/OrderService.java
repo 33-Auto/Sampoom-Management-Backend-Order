37998b35e0e39f55700d790a55bde882b8f1a2fc
Admin <admin@Adminui-MacBookPro-7.local>
2025-10-20 01:30:10 +0900

    delete: 필요없는 임포트

--- src/main/java/com/sampoom/backend/api/order/service/OrderService.java
+++ src/main/java/com/sampoom/backend/api/order/service/OrderService.java
@@ -12,7 +12,6 @@ import com.sampoom.backend.common.response.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
