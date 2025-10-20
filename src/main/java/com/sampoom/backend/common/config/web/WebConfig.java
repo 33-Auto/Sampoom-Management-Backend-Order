b326eec7638213651705bcd1cba458c2b2776930
Admin <admin@Adminui-MacBookPro-7.local>
2025-10-20 10:34:48 +0900

    feat: cors localhost:3000 허용

--- src/main/java/com/sampoom/backend/common/config/web/WebConfig.java
+++ src/main/java/com/sampoom/backend/common/config/web/WebConfig.java
@@ -17,6 +17,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://sampoom.store")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
