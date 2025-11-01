196b63e6e6d94c66dc0309e04cc420834518b245
Admin <admin@Adminui-MacBookPro-7.local>
2025-11-01 18:20:13 +0900

    fix: private 까먹음

--- src/main/java/com/sampoom/backend/api/order/event/OrderCancelEvent.java
+++ src/main/java/com/sampoom/backend/api/order/event/OrderCancelEvent.java
@@ -15,5 +15,5 @@ import java.util.List;
 public class OrderCancelEvent {
     private Long orderId;
     private Long warehouseId;
-    List<ItemBriefDto> parts;
+    private List<ItemBriefDto> parts;
 }
