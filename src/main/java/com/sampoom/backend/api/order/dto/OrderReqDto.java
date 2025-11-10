3d9cef1ea55c5ce570722367386d0faf5391014d
Admin <admin@Adminui-MacBookPro-7.local>
2025-11-10 22:28:34 +0900

    feat: agency id 필수

--- src/main/java/com/sampoom/backend/api/order/dto/OrderReqDto.java
+++ src/main/java/com/sampoom/backend/api/order/dto/OrderReqDto.java
@@ -1,5 +1,6 @@
 package com.sampoom.backend.api.order.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @ToString
 @Builder
 public class OrderReqDto {
+    @NotNull(message = "Agency ID는 필수입니다.")
     private Long agencyId;
     private String agencyName;
     private List<ItemCategoryDto> items;
