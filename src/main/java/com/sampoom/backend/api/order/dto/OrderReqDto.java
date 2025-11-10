281db751e188651a7b01cfbbbc8cb969a34fac2d
Admin <admin@Adminui-MacBookPro-7.local>
2025-11-10 22:36:32 +0900

    fix: 주문 입력 검증

--- src/main/java/com/sampoom/backend/api/order/dto/OrderReqDto.java
+++ src/main/java/com/sampoom/backend/api/order/dto/OrderReqDto.java
@@ -1,5 +1,6 @@
 package com.sampoom.backend.api.order.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +15,10 @@ import java.util.List;
 public class OrderReqDto {
     @NotNull(message = "Agency ID는 필수입니다.")
     private Long agencyId;
+
+    @NotNull(message = "Agency 지점명은 필수입니다.")
     private String agencyName;
+
+    @NotEmpty(message = "주문 항목은 최소 1개입니다.")
     private List<ItemCategoryDto> items;
 }
